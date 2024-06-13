ea349e5d39c7c4f170b8565ffdaf4fd97e6725bd
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-06-13 12:45:59 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ function App() {
     <div className="flex flex-col items-center mx-auto w-full font-medium text-white max-w-[1000px]">
       <img
         src={logo}
-        className="self-stretch w-full"
+        className="self-stretch w-[80%] m-auto"
       />
       <div className="mt-10 leading-5 text-center">
-        <h3 className="text-3xl">
+        <h3 className="text-3xl mb-5">
           ¡Te damos la bienvenida a  <mark className="px-2 text-white bg-[#EA5B6E] rounded">RobleAffinity, el oasis para graduados</mark>!
         </h3>
-        <div className="text-lg">
-          <p>En RobleAffinity, nos enfocaremos en brindar una experiencia única y enriquecedora para aquellos que han superado la treintena y cuenten con título(s) universitario(s).</p>
-          <p><mark className="px-2 text-white bg-[#EA5B6E] rounded">Prepárate para entrar en un mundo de infinitas posibilidades.</mark></p>
+        <div className="text-xl">
+          <p className="mb-3">En RobleAffinity, nos enfocaremos en brindar una experiencia única y enriquecedora para aquellos que han superado la treintena y cuenten con título(s) universitario(s).</p>
+          <p><mark className="text-2xl px-2 text-white bg-[#EA5B6E] rounded">Prepárate para entrar en un mundo de infinitas posibilidades.</mark></p>
         </div>
       </div>
       <div className="justify-center items-center px-16 py-4 mt-7 w-full text-base font-semibold tracking-normal border border-white border-solid max-w-[332px] rounded-[67.179px]">
         CREAR UNA CUENTA
       </div>
-      <div className="mt-7 text-sm leading-4 text-center underline">
+      <div className="mt-7 text-xl leading-4 text-center underline">
         ¿Ya tienes una cuenta? Inicia sesión
       </div>
     </div>
