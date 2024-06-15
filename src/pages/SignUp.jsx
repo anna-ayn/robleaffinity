52f5f57ac1ad487edaae39d9842179983e5f2103
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-06-15 13:44:34 -0400

    update

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,22 +1,23 @@
 import "../App.css";
 import logo from "../img/Logo.png";
+
 function SignUp() {
   return (
-    <div className="flex flex-col items-center mx-auto w-full font-medium text-white max-w-auto">
+    <div className="flex flex-col items-center mx-auto w-full h-full font-medium text-white max-w-auto">
       <img src={logo} className="self-stretch w-[30%] m-auto" />
-      <h3 className="text-3xl mb-5">
-        🌍 Conectando experiencias y sabiduría:{"  "}
-        <mark className="px-2 rounded text-4xl">
+      <h3 className="text-lg mb-5">
+        🌍 Conectando experiencias y sabiduría:{" "}
+        <mark className="px-1 rounded text-xl">
           Tu pareja ideal te espera 💖
         </mark>
         .
       </h3>
       <div className="p-5 h-full w-full bg-white rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
-        <form className="px-8 pt-6 pb-8 mb-4">
-          <div className="flex flex-row">
-            <div className="mb-4">
+        <form className="px-8 pt-6 pb-8 mb-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="mb-4 flex-grow">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-gray-700 text-sm font-bold mb-2 text-left"
                 htmlFor="name"
               >
                 Nombre
@@ -25,27 +26,78 @@ function SignUp() {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="name"
                 type="text"
-                placeholder="Username"
+                placeholder="Nombre"
+                required
               />
-              <div className="mb-4">
-                <label
-                  className="block text-gray-700 text-sm font-bold mb-2"
-                  htmlFor="surname"
-                >
-                  Apellido
-                </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="surname"
-                  type="text"
-                  placeholder="Username"
-                />
-              </div>
+            </div>
+            <div className="w-[1rem]"></div>
+            <div className="mb-4 flex-grow">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 text-left"
+                htmlFor="surname"
+              >
+                Apellido
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="surname"
+                type="text"
+                placeholder="Apellido"
+                required
+              />
             </div>
           </div>
-          <div className="mb-6">
+          <div className="flex flex-col md:flex-row justify-between">
+            <div className="mb-4 flex-grow">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 text-left"
+                htmlFor="birthdate"
+              >
+                Fecha de Nacimiento
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="birthdate"
+                type="date"
+                placeholder="Fecha de Nacimiento"
+                required
+              />
+            </div>
+            <div className="w-[1rem]"></div>
+            <div className="mb-4 flex-grow">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 text-left"
+                htmlFor="phone"
+              >
+                Teléfono
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="phone"
+                type="tel"
+                placeholder="Teléfono"
+                required
+              />
+            </div>
+          </div>
+          <div className="mb-4 flex-grow">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-700 text-sm font-bold mb-2 text-left"
+              htmlFor="email"
+            >
+              Correo electrónico
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="hola@ejemplo.com"
+              required
+            />
+          </div>
+          <div className="mb-4 flex-grow">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2 text-left"
               htmlFor="password"
             >
               Contraseña
@@ -55,6 +107,7 @@ function SignUp() {
               id="password"
               type="password"
               placeholder="********"
+              required
             />
           </div>
         </form>
