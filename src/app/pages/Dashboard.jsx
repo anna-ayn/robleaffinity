98249e45175c120e5d1b3cb00b917ac64a27a2df
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-07-15 19:35:39 -0400

    botones para el dashboard

--- src/app/pages/Dashboard.jsx
+++ src/app/pages/Dashboard.jsx
@@ -1,27 +1,6 @@
-import { useEffect, useState } from "react";
 import "../App.css";
 
-function Profile() {
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/getData", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    }).then((res) => {
-      if (res.error) {
-        alert(res.error);
-      } else {
-        res.json().then(async (res) => {
-          setUserData(res);
-        });
-      }
-    });
-  }, []);
-
+function Dashboard() {
   const logOut = () => {
     localStorage.removeItem("token");
     window.location.href = "/";
@@ -29,40 +8,24 @@ function Profile() {
 
   return (
     <>
-      {userData ? (
-        <div>
-          <h1>
-            Bienvenido, {userData.nombre} {userData.apellido}!
-          </h1>
-          <p>Tu correo es {userData.email}</p>
-          <p>Tu número de teléfono es {userData.telefono}</p>
-          <p>Tu fecha de nacimiento es {userData.fecha_nacimiento}</p>
-          <p>Tu idioma de la app es {userData.idioma}</p>
-          <p>
-            Tus notificaciones están {userData.notificaciones ? "ON" : "OFF"}
-          </p>
-          <p>Tu tema de la app es {userData.tema ? "Claro" : "Oscuro"}</p>
-          <p>Tu sexo es {userData.sexo}</p>
-          <p>Tu(s) foto(s) de perfil son:</p>
-          {userData.fotos.map((foto, index) => (
-            <img
-              key={index}
-              src={`data:image/jpg;base64,${foto}`}
-              className="w-[20%] m-auto"
-            />
-          ))}
-        </div>
-      ) : (
-        <p>Loading user data...</p>
-      )}
-      <button
-        onClick={logOut}
-        className="bg-[#de5466] hover:bg-[#e02841] text-white font-bold py-2 px-4 rounded"
-      >
-        Salir
-      </button>
+      <h1>Home</h1>
+      <p>¡Bienvenido!</p>
+      <p>¡Ya estás logueado!</p>
+
+      <div className="flex flex-col">
+        <button className="pulse m-3 p-3">Ver mi Perfil</button>
+        <button className="pulse m-3 p-3">
+          Configuracion del app y de preferencias
+        </button>
+        <button
+          onClick={logOut}
+          className="bg-[#de5466] hover:bg-[#e02841] text-white font-bold py-2 px-4 rounded"
+        >
+          Salir
+        </button>
+      </div>
     </>
   );
 }
 
-export default Profile;
+export default Dashboard;
