77652cd88b480af43ab5836cce495525cfcb5a31
Ana <73360276+anna-ayn@users.noreply.github.com>
2024-07-19 11:40:04 -0400

    update my profile

--- src/app/pages/MyProfile.jsx
+++ src/app/pages/MyProfile.jsx
@@ -484,11 +484,13 @@ function MyProfile() {
     <>
       <Sidebar />
       {userData ? (
-        <div className="bg-[#996ff242] backdrop-blur-xl shadow-xl  sm:ml-64 p-10">
+        <div className="bg-[#996ff242] backdrop-blur-xl shadow-xl  sm:ml-[15rem] p-10 w-[1000px]">
           <div className="flex flex-col sm:flex-row">
             <div className="flex w-1/3 h-full">
               <CarouselImgs
-                imgs={Object.values(userData?.fotos?.map((foto) => foto.r_foto))}
+                imgs={Object.values(
+                  userData?.fotos?.map((foto) => foto.r_foto)
+                )}
                 base64={true}
                 setIdx={setActualIndexPhoto}
               />
